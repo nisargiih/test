9c1db37dca10f5bce0ce66f290cdfe0017bd2cd9
nisargiih <nisarg.iihglobal@gmail.com>
2025-08-28 17:06:02 +0530

    Four point

--- index.js
+++ index.js
@@ -17,3 +17,12 @@ console.log("First push")
 console.log("second push")
 console.log("Third push")
 console.log("Four push")
+
+
+
+// ======================================== 4
+
+console.log("First push1")
+console.log("second push2")
+console.log("Third push3")
+console.log("Four push4")
